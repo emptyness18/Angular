34cfc6441d7566bd8238f098c8fc61eb3d654b16
David Cordero Chanto <emptyness18@hotmail.com>
2015-02-24 17:27:36 -0600

    Added module

---
+++ Angular/Scripts/module01.js
@@ -0,0 +1,14 @@
+﻿var myModule = angular.module('myModule', []);
+
+myModule.controller('myController', function ($scope) {
+    var data = { first: 'David', plan: 'basic-plan' };
+
+    $scope.data = data;
+});
+
+myModule.filter('stripDashes', function () {
+    return function (txt) {
+        return txt.split('-').join(' ');
+        //return txt.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() });
+    };
+});
