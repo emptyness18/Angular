bc70dbefeb9b096967ea81fe505d3ebdbc0d3635
David Cordero Chanto <emptyness18@hotmail.com>
2015-03-01 15:30:04 -0600

    Directives

---
+++ Angular/Directives.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Directives.aspx.cs" Inherits="Angular.Directives" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div ng-app="myModule" ng-controller="myController">
+        <input type="text" ng-model="textVal" />
+        <hs>Angular product</hs>
+        <div ng-bind="textVal"></div>    
+        <div ng-cloak>{{2+2}}</div>
+        <div ng-show="textVal == '1'">Es 1</div>
+        <div ng-hide="textVal == '1'">NO es 1</div>
+        <div ng-include="'Foot.html'"></div>
+        <!--div ng-repeat="x in ['first', 'second', 'third']">{{$index}} - {{x}}</!--div-->
+
+        <div color-list colors="colorsArray"></div>
+    </div>
+</asp:Content>
--- Angular/Scripts/module01.js
+++ Angular/Scripts/module01.js
@@ -4,11 +4,55 @@ myModule.controller('myController', function ($scope) {
     var data = { first: 'David', plan: 'basic-plan' };
 
     $scope.data = data;
+    $scope.textVal = "1";
+    $scope.colorsArray = ['red', 'blue', 'green'];
 });
 
 myModule.filter('stripDashes', function () {
     return function (txt) {
         return txt.split('-').join(' ');
         //return txt.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() });
+    };
+});
+
+myModule.directive('colorList', function ($compile) {
+    return {
+        restrict: 'AE',
+        template: "<button ng-click='showHiddenColors()' type='button'>"
+                + "{{isHidden? 'Show colors' : 'Hide colors'}}"
+                + "</button><div ng-hide='isHidden' id='colorContainer'>"
+                + "</div>",
+        link: function ($scope, $element) {
+            $scope.isHidden = true;
+            $scope.showHiddenColors = function () {
+                $scope.isHidden = !$scope.isHidden;
+            };
+
+            var colorContainer = $element.find('div');
+            console.log($scope.colorsArray);
+            angular.forEach($scope.colorsArray, function (color) {
+                var appendString = "<div style='background-color:" + color + "'>" + color + "</div>";
+                colorContainer.append(appendString);
+            });
+        }
+    };
+});
+
+myModule.directive('colorList2', function () {
+    return {
+        restrict: 'AE',
+        templateUrl: 'template1.html',
+        link: function ($scope, $element) {
+            $scope.isHidden = true;
+            $scope.showHiddenColors = function () {
+                $scope.isHidden = !$scope.isHidden;
+            };
+
+            var colorContainer = $element.find('div');
+            angular.forEach($scope.parent.colorsArray, function (color) {
+                var appendString = "<div style='background-color:" + color + "'>" + color + "</div>";
+                colorContainer.append(appendString);
+            });
+        }
     };
 });
